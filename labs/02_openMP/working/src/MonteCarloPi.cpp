84cd7a5000cfccce754f63866c63441641ef6f03
petrkucerak <petr@khome.cz>
2023-10-02 23:39:50 +0200

    Update the code

--- labs/02_openMP/working/src/MonteCarloPi.cpp
+++ labs/02_openMP/working/src/MonteCarloPi.cpp
@@ -1,37 +1,48 @@
-#include <random>
 #include <algorithm>
-#include <iostream>
-#include <vector>
-#include <omp.h>
 #include <cmath>
+#include <iostream>
+#include <omp.h>
+#include <random>
+#include <vector>
 
 using namespace std;
 
 // Do in parallel.
-int fnNumSamplesInCircle(int randomSeed, int numSamples) {
-    mt19937 randomEngine(randomSeed);
-    uniform_real_distribution<float> dist(-1.0, 1.0);
+int fnNumSamplesInCircle(int randomSeed, int numSamples)
+{
+   mt19937 randomEngine(randomSeed);
+   uniform_real_distribution<float> dist(-1.0, 1.0);
 
-    int numSamplesInCircle = 0;
-    // TODO:
+   int numSamplesInCircle = 0;
 
-    return numSamplesInCircle;
+   for (uint64_t i = 0; i < i < numSamples; ++i) {
+      float x = dist(randomEngine);
+      float y = dist(randomEngine);
+      (x * x + y * y) < 1 ? numSamplesInCircle += 1 : numSamplesInCircle += 0;
+   }
+
+   return numSamplesInCircle;
 }
 
-int main() {
-    int totalNumSamples = 1000000000;
+int main()
+{
+   int totalNumSamples = 100;
 
-    int numIterations = 100;
-    int numSamplesPerIteration = totalNumSamples / numIterations;
-    if ((totalNumSamples % numIterations) != 0) {
-        cout << "Total number of samples must be multiple of number of iterations!" << endl;
-        exit(1);
-    }
+   int numIterations = 100;
+   int numSamplesPerIteration = totalNumSamples / numIterations;
+   if ((totalNumSamples % numIterations) != 0) {
+      cout
+          << "Total number of samples must be multiple of number of iterations!"
+          << endl;
+      exit(1);
+   }
 
-    int numSamplesInCircle = 0;
+   int numSamplesInCircle = 0;
+   // TODO: not working
+#pragma omp parallel for reduction(+ : numSamplesInCircle)
+   for (uint64_t i = 0; i < numIterations; ++i)
+      numSamplesInCircle += fnNumSamplesInCircle(1, totalNumSamples);
 
-    // TODO: compute numSamplesInCircle in parallel
-
-    cout << (4.0 * numSamplesInCircle) / (1.0 * totalNumSamples) << endl;
-    return 0;
+   cout << (4.0 * numSamplesInCircle) / (1.0 * totalNumSamples) << endl;
+   return 0;
 }
