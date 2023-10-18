7006cc9d82657049720db4d0f0ee3b8f9f4d1ee2
petrkucerak <petr@khome.cz>
2023-10-18 10:00:34 +0200

    Define sending and receiving function

--- labs/03_openPM/working/src/SendAndReceive.cpp
+++ labs/03_openPM/working/src/SendAndReceive.cpp
@@ -8,22 +8,28 @@ int main(int argc, char **argv)
 {
    MPI_Init(&argc, &argv);
 
-   int myRank;
-   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+   int my_rank;
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-   if (myRank == 0) {
+   if (my_rank == 0) {
       // Sends message.
       string message = "IDDQD";
 
-      // TODO
-   } else if (myRank == 1) {
+      MPI_Send(message.c_str(), message.length() + 1, MPI_CHAR, 1, 0,
+               MPI_COMM_WORLD);
+
+   } else if (my_rank == 1) {
       // Receives message.
-      constexpr int bufSize = 256;
-      char buf[bufSize];
+      constexpr int buf_size = 256;
+      char buf[buf_size];
 
-      // TODO
+      MPI_Status status;
+      MPI_Recv(&buf, buf_size, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG,
+               MPI_COMM_WORLD, &status);
+      int received_size;
+      MPI_Get_count(&status, MPI_CHAR, &received_size);
 
-      cout << "Received message size: " << receivedSize << endl;
+      cout << "Received message size: " << received_size << endl;
       cout << "Received message content: " << string(buf) << endl;
    }
 
