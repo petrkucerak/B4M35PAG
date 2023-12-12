559520ed3daf469734d2227b20e30cf47b7254f2
petrkucerak <petr@khome.cz>
2023-12-12 13:02:24 +0100

    Define README.md

---
+++ hw/03_Bratley-Algo/working/README.md
@@ -0,0 +1,10 @@
+# HW3 - Parallel Branch and Bound: Bratley Algo
+
+## Script
+
+```sh
+cmake * # install dependecies
+make # compile binnary
+mpirun -np 4 ./Main # run binary woth mpi
+make && mpirun -np 4 ./Main # compile binay and run it
+```
--- hw/03_Bratley-Algo/working/src/Main.cpp
+++ hw/03_Bratley-Algo/working/src/Main.cpp
@@ -7,11 +7,11 @@ int main(int argc, char **argv)
 {
    // Initialize MPI
    MPI_Init(&argc, &argv);
-   int worldSize, myRank;
-   MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+   int world_size, my_rank;
+   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-   cout << "Hello wordl!" << endl;
+   cout << "Hello wordl from node " << my_rank << "!" << endl;
 
    MPI_Finalize();
    return 0;
