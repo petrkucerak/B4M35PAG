918cb46e098e7906c74b8244b5806afc40490ddd
petrkucerak <petr@khome.cz>
2023-12-19 18:53:01 +0100

    Add simple version

--- hw/03_Bratley-Algo/working/src/Main.cpp
+++ hw/03_Bratley-Algo/working/src/Main.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include <iostream>
 #include <mpi.h>
 #include <vector>
@@ -12,11 +13,33 @@ struct Task {
    int deadline;
 };
 
+/**
+ * @brief This struct is using in the dequeue
+ *
+ */
+struct Node {
+   int task_id;
+   int timestamp; /* The start of task */
+   int depth;
+};
+
 const void print_tasks(vector<Task> &tasks)
 {
    printf("\n");
    for (auto task : tasks) {
       printf("%d %d %d\n", task.process_time, task.release_time, task.deadline);
+   }
+}
+
+void addChildrens(deque<Node> &deque, vector<Task> &tasks, int depth,
+                  int task_id, int timestamp)
+{
+   int already_added = 0;
+   int new_children_count = tasks.size() - depth;
+   while (already_added < new_children_count) {
+      task_id != tasks.size() - 1 ? ++task_id : task_id = 0;
+      deque.push_back({task_id, timestamp, depth + 1});
+      ++already_added;
    }
 }
 
@@ -30,6 +53,7 @@ int main(int argc, char **argv)
 
    if (!rank) {
       // Load the metadata
+      bool is_find_solution = false;
       int task_count;
       if (scanf("%d\n", &task_count) != 1) {
          fprintf(stderr, "Can't load the task count\n");
@@ -52,7 +76,43 @@ int main(int argc, char **argv)
          tasks.push_back({process_time, release_time, deadline});
       }
 
-      print_tasks(tasks);
+      // TODO: handle if task is real,
+      // if deadline - process_time < release_time cause error
+
+      // print_tasks(tasks);
+
+      deque<Node> deque;
+      // Add first level to the end of deque
+      for (int i = 0; i < tasks.size(); ++i) {
+         deque.push_back({i, 0, 1});
+      }
+
+      while (!deque.empty()) {
+         // Get assignment from front of deque
+         Node node = deque.front();
+         Task task = tasks[node.task_id];
+         deque.pop_front();
+
+         // 1. test practicability of this element
+         int timestamp =
+             node.timestamp >= task.release_time
+                 ? node.timestamp
+                 : task.release_time + task.process_time; // end of current task
+         // Missed deadline
+         if (timestamp > task.deadline)
+            continue;
+
+         // Detect the end of timestamp
+         if (node.depth == task_count) {
+            is_find_solution = true;
+         }
+
+         // 2. add its children
+         addChildrens(deque, tasks, node.depth, node.task_id, timestamp);
+      }
+
+      if (!is_find_solution)
+         cout << "-1" << endl;
    }
 
    cout << "Hello wordl from node " << rank << "!" << endl;
